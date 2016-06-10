32b6361d73b867ba155990478abc8ba7192af1c7
cthorey <clement.thorey@gmail.com>
2016-06-10 14:30:54 +0200

    add compatibility python3

--- Oscar/Oscar.py
+++ Oscar/Oscar.py
@@ -4,60 +4,66 @@ import urllib2
 
 __all__ = ['Oscar', 'ClientException']
 
+
 class ClientException(Exception):
-  pass
+    pass
+
 
 class Oscar:
-  API_VERSION = '1'
-  #DOMAIN_URL = 'http://localhost:8080'
-  DOMAIN_URL = 'https://' + API_VERSION + '-dot-sensout-oscar.appspot.com'
+    API_VERSION = '1'
+    #DOMAIN_URL = 'http://localhost:8080'
+    DOMAIN_URL = 'https://' + API_VERSION + '-dot-sensout-oscar.appspot.com'
 
-  def __init__(self, access_token):
-    self.access_token = access_token
-    self.trial_ids = {}
-    self.trial_keys = {}
+    def __init__(self, access_token):
+        self.access_token = access_token
+        self.trial_ids = {}
+        self.trial_keys = {}
 
-  def call(self, url, params):
-    # prepare request
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + self.access_token}
-    url = url + '?' + urllib.urlencode(params)
-    req = urllib2.Request(self.DOMAIN_URL + url, None, headers)
-    try:
-      resp = urllib2.urlopen(req)
-      content = resp.read()
-      result = json.loads(content)
-      if result:
-        if 'redirect' in result:
-          raise ClientException("Your are not authenticated. You should try updating your access token.")
+    def call(self, url, params):
+        # prepare request
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': 'Bearer ' + self.access_token}
+        url = url + '?' + urllib.urlencode(params)
+        req = urllib2.Request(self.DOMAIN_URL + url, None, headers)
+        try:
+            resp = urllib2.urlopen(req)
+            content = resp.read()
+            result = json.loads(content)
+            if result:
+                if 'redirect' in result:
+                    raise ClientException(
+                        "Your are not authenticated. You should try updating your access token.")
+                else:
+                    return result
+            else:
+                raise ClientException(
+                    "Did not manage to parse API response: " + content)
+        except Exception as e:
+            print("Error when calling API:")
+            raise e
+
+    def get_job_hash(self, job):
+        return str(hash(json.dumps(job)))
+
+    def get_job_id(self, job):
+        return self.trial_ids[self.get_job_hash(job)]
+
+    def suggest(self, experiment):
+        result = self.call('/suggest', {
+                           'experiment': json.dumps(experiment)
+                           })
+        # result is non nil or error has been raised
+        if 'job' in result and 'trial_key' in result and 'trial_id' in result:
+            hash = self.get_job_hash(result['job'])
+            self.trial_ids[hash] = result['trial_id']
+            self.trial_keys[hash] = result['trial_key']
+            return result['job']
         else:
-          return result
-      else:
-        raise ClientException("Did not manage to parse API response: " + content)
-    except Exception as e:
-      print "Error when calling API:"
-      raise e
+            raise ClientException(
+                "No job returned. Check your parameters and quotas " + json.dumps(result))
 
-  def get_job_hash(self, job):
-    return str(hash(json.dumps(job)))
-
-  def get_job_id(self, job):
-    return self.trial_ids[self.get_job_hash(job)]
-
-  def suggest(self, experiment):
-    result = self.call('/suggest', {
-                       'experiment' : json.dumps(experiment)
-                       })
-    # result is non nil or error has been raised
-    if 'job' in result and 'trial_key' in result and 'trial_id' in result:
-      hash = self.get_job_hash(result['job'])
-      self.trial_ids[hash] = result['trial_id']
-      self.trial_keys[hash] = result['trial_key']
-      return result['job']
-    else:
-      raise ClientException("No job returned. Check your parameters and quotas " + json.dumps(result))
-
-  def update(self, job, result):
-    return self.call('/update', {
-                     'trial_key' : self.trial_keys[self.get_job_hash(job)],
-                     'result' : json.dumps(result)
-                     })
+    def update(self, job, result):
+        return self.call('/update', {
+                         'trial_key': self.trial_keys[self.get_job_hash(job)],
+                         'result': json.dumps(result)
+                         })
--- example.py
+++ example.py
@@ -1,20 +1,20 @@
 # set up Oscar
 from Oscar import Oscar
-ACCESS_TOKEN ='copy the access token from your Account panel here'
+ACCESS_TOKEN = 'copy the access token from your Account panel here'
 scientist = Oscar(ACCESS_TOKEN)
 
 # define your experiment hyperparameters' space here
 experiment = {
-  'name' : 'test',
-  'parameters' : {
-      'x' : { 'min' : -2, 'max' : 2 },
-      'c' : [ 3, 4, 5 ]
+    'name': 'test',
+    'parameters': {
+        'x': {'min': -2, 'max': 2},
+        'c': [3, 4, 5]
     }
-  }
+}
 
 # get a job suggestion
 job = scientist.suggest(experiment)
-print job
+print(job)
 
 # do your complex job here
 from random import randint
@@ -22,10 +22,10 @@ import math
 duration = randint(1, 100)
 loss = math.pow(job['x'] * job['c'], 2)
 result = {
-  'loss' : loss,
-  'duration' : duration
+    'loss': loss,
+    'duration': duration
 }
 
 # update Oscar
-print result
+print(result)
 scientist.update(job, result)
